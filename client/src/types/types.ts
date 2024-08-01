0a28cb09c049de165205a9ed82d4887c0b5355e0
shogadj <kuznetsovsemyondesu@gmail.com>
2024-08-01 14:40:37 +0300

    add: types&zod

---
+++ client/src/types/types.ts
@@ -0,0 +1,28 @@
+import type { z } from 'zod';
+import type { VideoSchema } from '../utils/validators';
+
+export type VideoType = z.infer<typeof VideoSchema>;
+
+export type LikeType = {
+  userId: number;
+  videoId: number;
+};
+export type UserVideoType = {
+  userId: number;
+  videoId: number;
+};
+
+export type UserType = {
+  id: number;
+  username: string;
+  email: string;
+};
+
+export type UserSignUpType = Omit<UserType, 'id'> & { password: string };
+export type UserSignInType = Omit<UserSignUpType, 'username'>;
+export type UserFromBackendType = { accessToken: string; user: UserType };
+
+export type UserStateType =
+  | { status: 'fetching' }
+  | { status: 'guest' }
+  | ({ status: 'logged' } & UserType);
