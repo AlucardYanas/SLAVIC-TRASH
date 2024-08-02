89e68d4ed3604d266cbae5d56e557bb2861d9322
ViktorLid <viks130293@gmail.com>
2024-08-02 15:21:26 +0300

    slicesDone

--- client/src/redux/update/updateSlice.ts
+++ client/src/redux/update/updateSlice.ts
@@ -1,23 +1,23 @@
 // redux/slices/uploadVideoSlice.ts
+
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import type { UploadVideoResponse } from '../../types/types';
 
 // API для загрузки видео
 export const uploadVideoApi = createApi({
   reducerPath: 'uploadVideoApi',
   baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
-  tagTypes: ['UploadVideo'],
+  tagTypes: ['UploadVideo', 'Video'], // Добавляем тег для видео
   endpoints: (builder) => ({
-    uploadVideo: builder.mutation<void, FormData>({
+    uploadVideo: builder.mutation<UploadVideoResponse, FormData>({
       query: (formData) => ({
         url: '/upload',
         method: 'POST',
         body: formData,
       }),
-      invalidatesTags: ['UploadVideo'],
+      invalidatesTags: [{ type: 'Video', id: 'LIST' }], // Инвалидируем кеш списка видео
     }),
   }),
 });
 
-export const {
-  useUploadVideoMutation,
-} = uploadVideoApi;
+export const { useUploadVideoMutation } = uploadVideoApi;
--- client/src/utils/validators.ts
+++ client/src/utils/validators.ts
@@ -1,13 +1,20 @@
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+// src/schemas/VideoSchema.ts
+
 import { z } from 'zod';
 
+// Определение схемы видео
 export const VideoSchema = z.object({
+  id: z.number(),
   title: z.string(),
-  link: z.string(),
+  videoPath: z.string(),
+  link: z.string().optional(), // Если `link` есть, но может быть пустым
   length: z.number(),
-  tags: z.array(z.string()),
+  tags: z.array(z.string()).optional(), // Массив тегов, который может быть пустым
+  approved: z.boolean(),
+  thumbnailPath: z.string().optional(), // Превью может отсутствовать
 });
 
 export const VideosSchema = z.array(VideoSchema);
