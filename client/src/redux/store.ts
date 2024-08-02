0e09248c4d95d4a52b0960a0d6676dd47c3edbc9
ViktorLid <viks130293@gmail.com>
2024-08-02 13:32:46 +0300

    createSlices

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,13 +1,23 @@
+// redux/store.ts
 import { configureStore } from '@reduxjs/toolkit';
 import { videosApi } from './apiSlice';
 import authSlice from './auth/authSlice';
+import { viewHistoryApi } from './slices/viewHistorySlice';
+import { uploadVideoApi } from './slices/uploadVideoSlice';
 
 export const store = configureStore({
   reducer: {
     [videosApi.reducerPath]: videosApi.reducer,
+    [viewHistoryApi.reducerPath]: viewHistoryApi.reducer,
+    [uploadVideoApi.reducerPath]: uploadVideoApi.reducer,
     auth: authSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(videosApi.middleware),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(
+      videosApi.middleware,
+      viewHistoryApi.middleware,
+      uploadVideoApi.middleware
+    ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
