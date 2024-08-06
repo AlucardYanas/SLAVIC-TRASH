7bae873e0471b66f61406b3a19213e7a8db3c712
Artem Pashkov <alucardyanas@gmail.com>
2024-08-06 19:06:28 +0300

    in process

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -10,21 +10,20 @@ import { store } from '../redux/store';
 export default function Layout(): JSX.Element {
   const status = useAppSelector((state) => state.auth.user.status);
 
-  // Define inline styles for the background and global body styles
   const layoutStyles: React.CSSProperties = {
     margin: 0,
     padding: 0,
     boxSizing: 'border-box',
     fontFamily: 'Arial, sans-serif',
-    backgroundColor: '#000', // Set a black background color
+    backgroundColor: '#000',
     backgroundImage: 'url("/94f1112fca6461da18e3d44752b18020.png")',
-    backgroundSize: '80% auto', // Keep the image size to 80% of the container width
-    backgroundPosition: 'center', // Center the background
-    backgroundRepeat: 'no-repeat', // No repeat
-    minHeight: '100vh', // Minimum height of the viewport
-    width: '100vw',  // Full width of the viewport
-    overflow: 'hidden', // Prevent overflow
-    display: 'flex',  // Use flex to manage layout
+    backgroundSize: '80% auto',
+    backgroundPosition: 'center',
+    backgroundRepeat: 'no-repeat',
+    minHeight: '100vh',
+    width: '100vw',
+    overflow: 'hidden',
+    display: 'flex',
     flexDirection: 'column',
   };
 
@@ -34,7 +33,7 @@ export default function Layout(): JSX.Element {
         <Loader isLoading={status === 'fetching'}>
           <Flex direction="column" minH="100vh">
             <Navbar />
-            <Box flex="1" overflow="auto"> {/* Allow overflow to be auto, preventing cut-off */}
+            <Box flex="1" overflow="auto">
               <Outlet />
             </Box>
           </Flex>
