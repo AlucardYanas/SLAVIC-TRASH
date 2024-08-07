b007984b7542ae72b0725e86e5ac212540539491
shogadj <kuznetsovsemyondesu@gmail.com>
2024-08-07 10:49:54 +0300

    typescriptFixes

--- client/src/redux/apiSlice.ts
+++ client/src/redux/apiSlice.ts
@@ -1,12 +1,16 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import type { VideoType } from '../types/types';
 
+export type ApiResponse<T> = {
+  data: T;
+}
+
 export const videosApi = createApi({
   reducerPath: 'api',
   baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
   tagTypes: ['Video'],
   endpoints: (builder) => ({
-    getVideos: builder.query<VideoType[], void>({
+    getVideos: builder.query<ApiResponse<VideoType[]>, void>({
       query: () => '/videos',
       providesTags: ['Video'],
     }),
