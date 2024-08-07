11096bc74c965f21f21e44cc49a83355e7b020db
Demetri Bis <26793601+zerthmonk@users.noreply.github.com>
2024-08-07 21:14:32 +0200

    add: local dockerization

---
+++ client/vite.config.docker.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://server:3000',
+    },
+  },
+  preview: {
+    port: 8080,
+  },
+});
