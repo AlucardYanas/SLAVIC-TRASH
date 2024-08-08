97caa70226f3c5567b7170812926f7b681124cc5
Artem Pashkov <alucardyanas@gmail.com>
2024-08-08 14:15:48 +0300

    fix: remooved one button from acc

--- client/src/components/pages/AccountPage.tsx
+++ client/src/components/pages/AccountPage.tsx
@@ -21,13 +21,13 @@ import {
   HStack,
 } from '@chakra-ui/react';
 import { FaTrash, FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-import type { AxiosError } from 'axios';
+
 
 import { useUploadVideoMutation } from '../../redux/upload/uploadSlice';
 import { useGetLikedVideosQuery, useUnlikeVideoMutation } from '../../redux/like/likeSlice';
 import VideoPlayer from '../ui/VideoPlayer';
 import type { RootState } from '../../redux/store';
-import type { VideoType } from '../../types/types';
+import type { VideoType, UploadVideoResponse } from '../../types/types';
 
 export default function AccountPage(): JSX.Element {
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
@@ -35,7 +35,7 @@ export default function AccountPage(): JSX.Element {
   const [showAlert, setShowAlert] = useState<boolean>(false);
   const [alertMessage, setAlertMessage] = useState<string | null>(null);
   const [alertStatus, setAlertStatus] = useState<'success' | 'error' | 'warning' | 'info'>('info');
-  const [uploadVideo, { isLoading }] = useUploadVideoMutation();
+  const [uploadVideo] = useUploadVideoMutation();
   const toast = useToast();
 
   const userId = useSelector((state: RootState) =>
@@ -55,10 +55,38 @@ export default function AccountPage(): JSX.Element {
 
   const [unlikeVideo] = useUnlikeVideoMutation();
 
-  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+  const handleFileChange = async (event: React.ChangeEvent<HTMLInputElement>): Promise<void> => {
     if (event.target.files && event.target.files[0]) {
       setSelectedFile(event.target.files[0]);
+
+      const formData = new FormData();
+      formData.append('video', event.target.files[0]);
+      formData.append('title', videoTitle);
+
+      const uploadPromise = uploadVideo(formData).unwrap();
+
+      const toastPromise = async (promise: Promise<UploadVideoResponse>, options: {
+        loading: { title: string, description: string },
+        success: { title: string, description: string },
+        error: { title: string, description: string }
+      }): Promise<void> => toast.promise(promise, options);
+
+      try {
+        await toastPromise(uploadPromise, {
+          loading: { title: 'Видос грузится в облако', description: 'Положди чуть-чуть, братан' },
+          success: { title: 'Ништяк!', description: 'Видос Подгружен, Мы его посмотрим и добавим, если всё ок.' },
+          error: { title: 'Произошла лажа', description: 'Не могу грузануть' },
+        });
+        setSelectedFile(null);
+        setVideoTitle('');
+      } catch (err) {
+        console.error('Ошибка загрузки:', err);
+      }
     }
+  };
+
+  const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    void handleFileChange(event); // Оборачиваем вызов асинхронной функции
   };
 
   const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
@@ -68,93 +96,6 @@ export default function AccountPage(): JSX.Element {
   const isUndesirableContent = (title: string): boolean => {
     const undesirableKeywords = ['bad', 'offensive', 'undesirable'];
     return undesirableKeywords.some((keyword) => title.toLowerCase().includes(keyword));
-  };
-
-  const handleSubmit = async (): Promise<void> => {
-    if (!selectedFile || !videoTitle || userId === null) {
-      if (!selectedFile && !videoTitle) {
-        toast({
-          title: 'Ошибка!',
-          description: 'Пожалуйста, выберите файл видео и введите название.',
-          status: 'error',
-          duration: 5000,
-          isClosable: true,
-        });
-      } else if (!selectedFile) {
-        toast({
-          title: 'Ошибка!',
-          description: 'Пожалуйста, выберите файл видео.',
-          status: 'error',
-          duration: 5000,
-          isClosable: true,
-        });
-      } else if (!videoTitle) {
-        toast({
-          title: 'Ошибка!',
-          description: 'Пожалуйста, введите название видео.',
-          status: 'error',
-          duration: 5000,
-          isClosable: true,
-        });
-      }
-      setAlertMessage(
-        'Пожалуйста, выберите файл видео, введите название и убедитесь, что вы авторизованы.',
-      );
-      setAlertStatus('info');
-      setShowAlert(true);
-      return;
-    }
-
-    if (isUndesirableContent(videoTitle)) {
-      setAlertMessage('Видео содержит нежелательный контент и не может быть загружено.');
-      setAlertStatus('warning');
-      setShowAlert(true);
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append('video', selectedFile);
-    formData.append('title', videoTitle);
-
-    try {
-      await uploadVideo(formData).unwrap();
-      setAlertMessage('Видео успешно загружено.');
-      setAlertStatus('success');
-      setSelectedFile(null);
-      setVideoTitle('');
-
-      toast({
-        title: 'Успех!',
-        description: 'Мы его посмотрим и добавим, если всё ок.',
-        status: 'success',
-        duration: 5000,
-        isClosable: true,
-      });
-    } catch (err: unknown) {
-      console.error('Ошибка загрузки:', err);
-
-      toast({
-        title: 'Произошла лажа',
-        description: 'Не могу грузануть(',
-        status: 'error',
-        duration: 5000,
-        isClosable: true,
-      });
-
-      if ((err as AxiosError).status === 400) {
-        setAlertMessage('Видео содержит нежелательный контент и не может быть загружено.');
-        setAlertStatus('warning');
-      } else {
-        setAlertMessage('Не удалось загрузить видео. Пожалуйста, попробуйте еще раз.');
-        setAlertStatus('error');
-      }
-    } finally {
-      setShowAlert(true);
-    }
-  };
-
-  const handleSubmitClick = (): void => {
-    void handleSubmit();
   };
 
   const handleVideoSelect = (index: number): void => {
@@ -177,14 +118,14 @@ export default function AccountPage(): JSX.Element {
       });
     }
   };
-  
 
   const handlePrevVideo = (): void => {
     if (likedVideos && likedVideos.length > 0) {
       setCurrentVideoIndex((prevIndex) => {
         if (prevIndex - 3 >= 0) {
           return prevIndex - 3;
-        } return 0;
+        }
+        return 0;
       });
     }
   };
@@ -231,7 +172,7 @@ export default function AccountPage(): JSX.Element {
         type="file"
         accept="video/*"
         style={{ display: 'none' }}
-        onChange={handleFileChange}
+        onChange={handleFileInputChange}
       />
 
       <Flex direction="column" alignItems="center" mb={4}>
@@ -251,18 +192,7 @@ export default function AccountPage(): JSX.Element {
           background="#DD6B20"
           onClick={() => document.getElementById('fileInput')?.click()}
         >
-          {selectedFile ? 'Видео выбрано' : 'Выбрать видео'}
-        </Button>
-        <Button
-          size="lg"
-          variant="solid"
-          colorScheme="gray"
-          background="#DD6B20"
-          onClick={handleSubmitClick}
-          isLoading={isLoading}
-          mt={2}
-        >
-          Загрузить видео
+          Выбрать видео и загрузить
         </Button>
       </Flex>
 
@@ -287,63 +217,63 @@ export default function AccountPage(): JSX.Element {
         {!isLoadingLikedVideos && error && <Text>Ошибка при загрузке данных.</Text>}
         {!isLoadingLikedVideos && !error && (
           <Flex align="center" justify="center" position="relative" w="full" maxW="1200px">
-          <IconButton
-            icon={<FaChevronLeft />}
-            aria-label="Previous Video"
-            onClick={handlePrevVideo}
-            position="absolute"
-            left="-60px"
-            zIndex="1"
-            variant="solid"
-            colorScheme="orange"
-            size="xl"
-            _hover={{ opacity: 0.7 }}
-          />
-          <HStack spacing={4} overflow="hidden" w="full" justifyContent="center">
-            {likedVideos.slice(currentVideoIndex, currentVideoIndex + 3).map((video, index) => (
-              <Box key={video.id} position="relative" w="380px">
-                <AspectRatio ratio={16 / 9}>
-                  <Image
-                    src={video.thumbnailPath}
-                    alt={video.title}
-                    boxSize="full"
-                    objectFit="cover"
-                    onClick={() => handleVideoSelect(currentVideoIndex + index)}
-                    cursor="pointer"
+            <IconButton
+              icon={<FaChevronLeft />}
+              aria-label="Previous Video"
+              onClick={handlePrevVideo}
+              position="absolute"
+              left="-60px"
+              zIndex="1"
+              variant="solid"
+              colorScheme="orange"
+              size="xl"
+              _hover={{ opacity: 0.7 }}
+            />
+            <HStack spacing={4} overflow="hidden" w="full" justifyContent="center">
+              {likedVideos.slice(currentVideoIndex, currentVideoIndex + 3).map((video, index) => (
+                <Box key={video.id} position="relative" w="380px">
+                  <AspectRatio ratio={16 / 9}>
+                    <Image
+                      src={video.thumbnailPath}
+                      alt={video.title}
+                      boxSize="full"
+                      objectFit="cover"
+                      onClick={() => handleVideoSelect(currentVideoIndex + index)}
+                      cursor="pointer"
+                    />
+                  </AspectRatio>
+                  <Text noOfLines={2} mt={2} fontSize="lg" fontWeight="medium">
+                    {video.title}
+                  </Text>
+                  <IconButton
+                    aria-label="Удалить из избранного"
+                    icon={<FaTrash />}
+                    colorScheme="red"
+                    size="sm"
+                    position="absolute"
+                    top="4"
+                    right="4"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      void handleUnlike(video.id);
+                    }}
                   />
-                </AspectRatio>
-                <Text noOfLines={2} mt={2} fontSize="lg" fontWeight="medium">
-                  {video.title}
-                </Text>
-                <IconButton
-                  aria-label="Удалить из избранного"
-                  icon={<FaTrash />}
-                  colorScheme="red"
-                  size="sm"
-                  position="absolute"
-                  top="4"
-                  right="4"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    void handleUnlike(video.id);
-                  }}
-                />
-              </Box>
-            ))}
-          </HStack>
-          <IconButton
-            icon={<FaChevronRight />}
-            aria-label="Next Video"
-            onClick={handleNextVideo}
-            position="absolute"
-            right="-60px"
-            zIndex="1"
-            variant="solid"
-            colorScheme="orange"
-            size="xl"
-            _hover={{ opacity: 0.7 }}
-          />
-        </Flex>
+                </Box>
+              ))}
+            </HStack>
+            <IconButton
+              icon={<FaChevronRight />}
+              aria-label="Next Video"
+              onClick={handleNextVideo}
+              position="absolute"
+              right="-60px"
+              zIndex="1"
+              variant="solid"
+              colorScheme="orange"
+              size="xl"
+              _hover={{ opacity: 0.7 }}
+            />
+          </Flex>
         )}
       </Flex>
 
